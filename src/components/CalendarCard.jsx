26fef4df267d4078f75c08f2b2e1beb00399b697
paili2 <zxcv7502@naver.com>
2025-04-29 14:40:35 +0900

    　

--- src/components/CalendarCard.jsx
+++ src/components/CalendarCard.jsx
@@ -1,7 +1,7 @@
 const ClaendarCard = () => {
   const date = new Date().toGMTString().split(" ");
   return (
-    <div border rounded-lg shadow-md p-3 bg-white>
+    <div className="border rounded-lg shadow-md p-3 bg-white">
       {date
         .slice(0, 4)
         .map((v) => v)
