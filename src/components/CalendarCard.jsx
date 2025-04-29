5c5d7000ae0282916c9875687b1bdbf7611ce53c
paili2 <zxcv7502@naver.com>
2025-04-29 15:34:40 +0900

    　

--- src/components/CalendarCard.jsx
+++ src/components/CalendarCard.jsx
@@ -1,11 +1,16 @@
 const ClaendarCard = () => {
-  const date = new Date().toGMTString().split(" ");
+  const date = new Date()
+    .toLocaleString("en-US", { hour12: false })
+    .replace(",", " ")
+    .replaceAll("/", ".")
+    .split(" ");
+
   return (
-    <div className="border rounded-lg shadow-md p-3 bg-white">
-      {date
-        .slice(0, 4)
-        .map((v) => v)
-        .join(" ")}
+    <div className="border rounded-lg shadow-md p-5 bg-white">
+      <div className="text-lg font-semibold text-blue-600 mb-1">
+        {date[0].split(".").join(". ")}
+      </div>
+      <span className="text-5xl text-gray-800 font-bold">{date[2]}</span>
     </div>
   );
 };
