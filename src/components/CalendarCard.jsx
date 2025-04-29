a5edcf65af03c76ee47496c7cc6b348d2b5124b5
paili2 <zxcv7502@naver.com>
2025-04-29 14:37:22 +0900

    　

---
+++ src/components/CalendarCard.jsx
@@ -0,0 +1,13 @@
+const ClaendarCard = () => {
+  const date = new Date().toGMTString().split(" ");
+  return (
+    <div border rounded-lg shadow-md p-3 bg-white>
+      {date
+        .slice(0, 4)
+        .map((v) => v)
+        .join(" ")}
+    </div>
+  );
+};
+
+export default ClaendarCard;
--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,7 +1,14 @@
-const ListCard = (props) => {
+import { Children } from "react";
+
+const ListCard = ({ height, title, text, children }) => {
   return (
-    <div className="w-1/2 border rounded-sm shadow-md">
-      <h2 className="pb-3 border-b-1">{props.title}</h2>
+    <div
+      className={`w-full ${
+        title.toUpperCase().includes("LIST") ? "h-[500px]" : "h-fit"
+      } border-0 rounded-4xl shadow-md p-7 bg-white font-bold text-2xl`}
+    >
+      <h2 className="pb-3 border-b-1">{title}</h2>
+      <span>{children}</span>
     </div>
   );
 };
