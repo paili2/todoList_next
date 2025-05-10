5abdcee38142c4e8e7e3e8c21931f974ef10dfb8
paili2 <zxcv7502@naver.com>
2025-05-10 15:25:26 +0900

    　

--- src/components/CalendarCard.jsx
+++ src/components/CalendarCard.jsx
@@ -1,16 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const ClaendarCard = () => {
-  const date = new Date()
-    .toLocaleString("en-US", { hour12: false })
-    .replace(",", " ")
-    .replaceAll("/", ".")
-    .split(" ");
+  const [calendar, setCalendar] = useState("");
+  const [time, setTime] = useState("");
+  useEffect(() => {
+    setInterval(() => {
+      const [year, month, day, hour, minute, seconds] = new Date()
+        .toLocaleString("ko-KR", {
+          hour12: false,
+        })
+        .split(" ");
+
+      setCalendar((prev) => year + month + day);
+      setTime(
+        (prev) =>
+          hour.replace("시", ":").padStart(2, "0") +
+          minute.replace("분", ":").padStart(2, "0") +
+          seconds.replace("초", "").padStart(2, "0")
+      );
+    }, 1000);
+  }, []);
 
   return (
     <div className="border rounded-lg shadow-md p-5 bg-white">
-      <div className="text-lg font-semibold text-blue-600 mb-1">
-        {date[0].split(".").join(". ")}
-      </div>
-      <span className="text-5xl text-gray-800 font-bold">{date[2]}</span>
+      <div className="text-lg font-semibold text-blue-600 mb-1">{calendar}</div>
+      <span className="text-5xl text-gray-800 font-bold">{time}</span>
     </div>
   );
 };
